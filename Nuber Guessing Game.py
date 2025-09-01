45572e810fc29fda198d8fadb83a012248b8b2e9
Kunal Das <wastematerial12345@gmail.com>
2025-09-01 05:13:05 +0000

    game

---
+++ Nuber Guessing Game.py
@@ -0,0 +1,12 @@
+import random
+
+def number_guessing_game():
+    number_to_guess = random.randint(1, 100)
+    guess = None
+    while guess != number_to_guess:
+        guess = int(input("Guess the number between 1 and 100"))
+        if guess < number_to_guess:
+            print("Too low")
+        elif guess > number_to_guess:
+            print("Too high")
+    print("YOU WIN")
